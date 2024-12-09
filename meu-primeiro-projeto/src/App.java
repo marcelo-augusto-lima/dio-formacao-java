a0c4aae82ae5e3fbddefb53f4e561e06757af063
marcelo-augusto-lima <mar.augusto.albano.lima@gmail.com>
2024-12-08 21:59:45 -0300

    correções

--- meu-primeiro-projeto/src/App.java
+++ meu-primeiro-projeto/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Olá, é um prazer conhecelos!");
+        System.out.println("Olá, é um prazer conhecer a todos!");
     }
 }
