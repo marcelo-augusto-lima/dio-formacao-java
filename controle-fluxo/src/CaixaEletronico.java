c1a760a8e74c099451eb513514f922234866a3de
marcelo-augusto-lima <mar.augusto.albano.lima@gmail.com>
2025-02-01 16:55:26 -0300

    Adicionado exemplos de condicional composta

--- controle-fluxo/src/CaixaEletronico.java
+++ controle-fluxo/src/CaixaEletronico.java
@@ -1,11 +1,16 @@
 public class CaixaEletronico {
     public static void main(String[] args) {
         double saldo = 25.0;
-        double valorSolicitado = 17.0;
+        double valorSolicitado = 26.0;
 
-        if(valorSolicitado < saldo)
+        if(valorSolicitado < saldo){
             saldo -= valorSolicitado;
+            System.out.println("Saque realizado com sucesso!");
+        }
+        else{
+            System.out.println("Saldo insuficiente!");
+        }
             
-        System.out.println(saldo);
+        System.out.println("Saldo atual " + saldo);
     }
 }
---
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -0,0 +1,12 @@
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+        double nota = 5.0;
+
+        if (nota >= 6){
+            System.out.println("Aprovado");
+        }
+        else{
+            System.out.println("Reprovado");
+        }
+    }
+}
