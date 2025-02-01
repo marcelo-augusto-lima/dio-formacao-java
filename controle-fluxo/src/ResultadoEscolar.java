89c14060d4bf114430794901c3353f351e1e3222
marcelo-augusto-lima <mar.augusto.albano.lima@gmail.com>
2025-02-01 17:00:26 -0300

    Adicionado estrutura de condicional encadeada

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -2,8 +2,11 @@ public class ResultadoEscolar {
     public static void main(String[] args) {
         double nota = 5.0;
 
-        if (nota >= 6){
+        if (nota >= 7){
             System.out.println("Aprovado");
+        }
+        else if(nota >= 5 && nota < 7){
+            System.out.println("Recuperação");
         }
         else{
             System.out.println("Reprovado");
