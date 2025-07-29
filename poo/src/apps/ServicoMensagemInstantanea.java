6c9fc6254269a9262d6ad0e0534ad6166f9fe949
marcelo-augusto-lima <mar.augusto.albano.lima@gmail.com>
2025-07-28 21:47:05 -0300

    Adicionado exemplos da orientação a objetos com programa de mensagens

---
+++ poo/src/apps/ServicoMensagemInstantanea.java
@@ -0,0 +1,11 @@
+package apps;
+
+public abstract class ServicoMensagemInstantanea {
+    public abstract void enviarMensagem();
+
+    public abstract void receberMensagem();
+
+    protected void validarConectadoInternet(){
+        System.out.println("Validando se está conectado a internet");
+    }
+}
--- poo/src/msn/ComputadorPedrinho.java
+++ poo/src/msn/ComputadorPedrinho.java
@@ -1,10 +1,24 @@
 package msn;
 
+import apps.ServicoMensagemInstantanea;
+import apps.MSNMessenger;
+import apps.FacebookMessenger;
+import apps.Telegram;
+
 public class ComputadorPedrinho {
     public static void main(String[] args) {
-        MSNMessenger msn = new MSNMessenger();
+        ServicoMensagemInstantanea smi = null;
 
-        msn.enviarMensagem();
-        msn.receberMensagem();
+        String appEscolhido = "tlg";
+
+        if(appEscolhido.equals("msn"))
+            smi = new MSNMessenger();
+        else if(appEscolhido.equals("fbm"))
+            smi = new FacebookMessenger();
+        else if(appEscolhido.equals("tlg"))
+            smi = new Telegram();
+
+        smi.enviarMensagem();
+        smi.receberMensagem();
     }
 }
